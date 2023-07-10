4666742b8d44a817134b92a436cd5f1b6da08d8d
Swapnil639 <swapnilpatil631997@gmail.com>
2023-07-10 10:13:24 +0530

    Added :Create MenuItem and Restaurants class maintain information for each resto classes

--- src/com/bridgelabz/Restaurant.java
+++ src/com/bridgelabz/Restaurant.java
@@ -1,2 +1,40 @@
-package com.bridgelabz;public class Restaurant {
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Restaurant {
+    private String name;
+    private List<MenuItem> menuItems;
+
+    public Restaurant(String name) {
+        this.name = name;
+        menuItems = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addMenuItem(MenuItem item) {
+        menuItems.add(item);
+    }
+
+    public void removeMenuItem(MenuItem item) {
+        menuItems.remove(item);
+    }
+
+    public List<MenuItem> getMenu() {
+        return menuItems;
+    }
+
+    public MenuItem searchMenu(String name) {
+        for (MenuItem item : menuItems) {
+            if (item.getName().equals(name)) {
+                return item;
+            }
+        }
+        return null;
+    }
 }
+
--- src/com/bridgelabz/RestaurantMain.java
+++ src/com/bridgelabz/RestaurantMain.java
@@ -1,5 +1,40 @@
 package com.bridgelabz;
 
-public class Main {
-    
+public class RestaurantMain {
+    public static void main(String[] args) {
+        Restaurant mcDonalds = new Restaurant("McDonald's");
+        Restaurant burgerKing = new Restaurant("Burger King");
+
+        mcDonalds.addMenuItem(new MenuItem("Big Mac", 5.99));
+        mcDonalds.addMenuItem(new MenuItem("French Fries", 2.99));
+        burgerKing.addMenuItem(new MenuItem("Whopper", 6.99));
+        burgerKing.addMenuItem(new MenuItem("Onion Rings", 3.99));
+
+        System.out.println("Menus of " + mcDonalds.getName() + ":");
+        for (MenuItem item : mcDonalds.getMenu()) {
+            System.out.println(item.getName() + " - " + item.getPrice());
+        }
+        System.out.println("Menus of " + burgerKing.getName() + ":");
+        for (MenuItem item : burgerKing.getMenu()) {
+            System.out.println(item.getName() + " - " + item.getPrice());
+        }
+
+        MenuItem item = mcDonalds.searchMenu("Big Mac");
+        if (item != null) {
+            System.out.println("Found " + item.getName() + " in " + mcDonalds.getName() + " for " + item.getPrice());
+        } else {
+            System.out.println("Menu item not found in " + mcDonalds.getName());
+        }
+
+        MenuItem onionRings = burgerKing.searchMenu("Onion Rings");
+        if (onionRings != null) {
+            burgerKing.removeMenuItem(onionRings);
+            System.out.println("Removed " + onionRings.getName() + " from " + burgerKing.getName());
+        }
+
+        System.out.println("Menus of " + burgerKing.getName() + " after removing Onion Rings:");
+        for (MenuItem m : burgerKing.getMenu()) {
+            System.out.println(m.getName() + " - " + m.getPrice());
+        }
+    }
 }
